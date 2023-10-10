2e5f59831c5dd5431cfe54ebcbe6268f49967b36
Usama Rahman <rahmanusama390@gmail.com>
2023-10-11 00:22:31 +0600

    💯

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNunmerAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [Password, setPassword] = useState("");
+
+  const PasswordGenerator = useCallback( () => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  
+    if(numberAllowed) str += "0123456789";
+    if(charAllowed) str += "/@#$%&*!~";
+  
+  } ,[length,numberAllowed,charAllowed,setPassword]);
+
+
+
+  
   return (
     <>
-    
+    <h1 className='text-4xl'>Password Generator</h1>
     </>
   )
 }
